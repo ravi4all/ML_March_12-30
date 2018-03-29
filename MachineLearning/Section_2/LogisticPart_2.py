6f04ae12fc971942ed784d7da4846edebc331c49
ravi4all <ravikanttyagi.2295@gmail.com>
2018-03-29 13:45:03 +0530

    Add files via upload

--- MachineLearning/Section_2/LogisticPart_2.py
+++ MachineLearning/Section_2/LogisticPart_2.py
@@ -1,14 +1,57 @@
 import csv
+import math
+import random
 
-file = open('pima-indians-diabetes.data.csv')
-reader = csv.reader(file)
-dataset = []
-for data in reader:
-    dataset.append(data)
+def load_data(filename):
+    dataset = []
+    
+    with open(filename,'r') as file:
+        reader = csv.reader(file)
+        
+        for row in reader:
+            dataset.append(row)
+    
+    return dataset
 
-X = []
-y = []
+def str_to_float(dataset):
+    
+    for row in range(len(dataset)):
+        for col in range(len(dataset[0])):
+            dataset[row][col] = float(dataset[row][col].strip())
 
-for i in dataset:
-    X.append(i[:-1])
-    y.append(i[-1])
+def minmax_data(dataset):
+    minmax = []
+    
+    for i in range(len(dataset[0])):
+        col_values = [row[i] for row in dataset]
+        min_value = min(col_values)
+        max_value = max(col_values)
+        minmax.append([min_value, max_value])
+    
+    return minmax
+
+def normalize_data():
+    pass
+
+def cross_validation():
+    pass
+
+def accuracy_score():
+    pass
+
+def evaluate_alogorithm():
+    pass
+
+def predict():
+    pass
+
+def sgd_logistic():
+    pass
+
+def logistic_regression():
+    pass
+
+filename = 'pima-indians-diabetes.data.csv'
+dataset = load_data(filename)
+str_to_float(dataset)
+minmax = minmax_data(dataset)
