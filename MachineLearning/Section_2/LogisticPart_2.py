f424a49d876b1daf5836494b5dde7b9a523c791d
ravi4all <ravikanttyagi.2295@gmail.com>
2018-04-02 12:56:34 +0530

    Add files via upload

--- MachineLearning/Section_2/LogisticPart_2.py
+++ MachineLearning/Section_2/LogisticPart_2.py
@@ -30,11 +30,28 @@ def minmax_data(dataset):
     
     return minmax
 
-def normalize_data():
-    pass
+def normalize_data(dataset, minmax):
+    numer = 0
+    denom = 0
+    
+    for row in dataset:
+        for i in range(len(row)):
+            numer = row[i] - minmax[i][0]
+            denom = minmax[i][1] - minmax[i][0]
+            row[i] = numer/denom
 
-def cross_validation():
-    pass
+def cross_validation(dataset, n_folds):
+    dataset_split = []
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = []
+        while len(fold) < fold_size:
+            index = random.randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    
+    return dataset_split
 
 def accuracy_score():
     pass
@@ -55,3 +72,7 @@ filename = 'pima-indians-diabetes.data.csv'
 dataset = load_data(filename)
 str_to_float(dataset)
 minmax = minmax_data(dataset)
+normalize_data(dataset, minmax)
+
+#c = cross_validation(dataset, 5)
+e = cross_validation(dataset, 5)
